3ffe8fd0a5f8f570962f3c8f40e9a0e41b8469e9
Alphaboy20023 <timivictor565@gmail.com>
2025-06-18 17:50:02 +0200

    changes

--- school_inventory/settings.py
+++ school_inventory/settings.py
@@ -24,10 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-6n2k2*$x3f$ly4#-l_9ta+$q3)$+9xhqr3pp947i5thmk++svb')
 
 
-DEBUG = os.environ.get('DEBUG', 'FALSE')  == 'True'
+DEBUG=True
 
 # * allow any host
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "localhost").split(" ")
+ALLOWED_HOSTS = [
+    'inventory-management-solution-production.up.railway.app',
+    '127.0.0.1',
+    'localhost',
+]
 
 
 
